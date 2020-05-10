5923ae574a90648488fda8995494478920d6e69f
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2020-05-10 16:54:14 -0500

    rendering almost working

--- render.py
+++ render.py
@@ -25,8 +25,16 @@ def get_posttitle(filename):
     with open(filename) as f:
         for line in f:
             if 'class="posttitle"' in line:
-                name = get_name(line)
+                name = get_name(line).strip()
                 return name
+
+
+def populate_links(names):
+    l = []
+    for postname, postfile in names:
+        s = f'<li><a href="{postfile}">{postname}</a></li>'
+        l.append(s)
+    return '\n'.join(l)
 
 
 if __name__ == '__main__':
@@ -53,29 +61,33 @@ if __name__ == '__main__':
         reverse=True
     )
 
-    posts = sorted(
+    posts = sorted((
         f'posts/{post}.html'
         for fname in to_render
-        for post in [fname.split('.')[0]]
+        for post in [fname.split('.')[0]]),
+        reverse=True
     )
 
     names = [
-        get_posttitle(os.path.join('templates', post))
+        (get_posttitle(os.path.join('templates', post)), os.path.join('posts', post))
         for post in to_render
     ]
 
     items = list(zip(to_render, posts, names))
     print('rendering:', items)
     first = True
+    links = populate_links(names)
+    print('links =', links)
     for filename, post, name in items:
         with open(os.path.join('templates', filename)) as f:
             lines = f.readlines()
-        post_contents = template[:i] + lines + template[i+1:]
+        post_contents = template[:post_index] + lines + template[post_index+1:links_index] + [links] + template[links_index+1:]
         text = ''.join(post_contents)
 
         with open(post, 'w') as f:
             f.write(text)
         if first:
+            first = False
             with open(f'index.html', 'w') as f:
                 f.write(text)
-                links = populate_lines(names, filename)
+
