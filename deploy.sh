2532597b6fb9f44c867faa3359a1ea74a448fb6d
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2020-05-13 18:14:03 -0500

    one script deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,18 @@
+set -e
+
+COMMIT_MSG=$1
+if [[ -z $COMMIT_MSG ]]; then
+    echo "missing commit message"
+    exit 1
+fi
+
+echo "commit message: $COMMIT_MSG"
+
 cp *.html ../kkloberdanz.github.io/
 cp -r style/ ../kkloberdanz.github.io/
 cp -r image/ ../kkloberdanz.github.io/
+
+cd ../kkloberdanz.github.io
+git add --all
+git commit -m "$COMMIT_MSG"
+git push
